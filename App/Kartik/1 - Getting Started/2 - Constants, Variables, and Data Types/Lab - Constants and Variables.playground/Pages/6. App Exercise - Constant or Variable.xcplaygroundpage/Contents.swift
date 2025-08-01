f37c043d2a69d4aad6ccf9ea5d28384919d91fe4
Kartik Sharma <Kartiksharma15.07.2004@gmail.com>
2025-08-01 13:54:38 +0530

    push
    push -->

---
+++ App/Kartik/1 - Getting Started/2 - Constants, Variables, and Data Types/Lab - Constants and Variables.playground/Pages/6. App Exercise - Constant or Variable.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+let userName = "Alex"
+let userAge = 25
+var stepsToday = 4300
+let dailyStepGoal = 10000
+var averageHeartRate = 76.5 
+
+/*:
+ Now go back and add a line after each constant or variable declaration. On those lines, print a statement explaining why you chose to declare the piece of information as a constant or variable.
+ 
+[Previous](@previous)  |  page 6 of 10  |  [Next: Exercise - Types and Type Safety](@next)
+ */
