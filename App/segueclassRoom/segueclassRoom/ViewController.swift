0352ea8ddd7f277d561ccc71de64c8e4479838c0
Kartik07Sharma <kartik2032.be23@chitkara.edu.in>
2025-08-01 14:12:38 +0530

    Add files via upload
    all donee yeahhhh

---
+++ App/segueclassRoom/segueclassRoom/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  segueclassRoom
+//
+//  Created by Kartik Sharma on 18/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func
+    unwindToRed(unwindSegue: UIStoryboardSegue){
+        
+    }
+}
+
---
+++ App/segueclassRoom/segueclassRoomTests/segueclassRoomTests.swift
@@ -0,0 +1,17 @@
+//
+//  segueclassRoomTests.swift
+//  segueclassRoomTests
+//
+//  Created by Kartik Sharma on 18/07/25.
+//
+
+import Testing
+@testable import segueclassRoom
+
+struct segueclassRoomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
